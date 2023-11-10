c4197d8a31ee438938b00ddf60f9dd9fb15ab609
DarkCloudCZ <94063126+DarkCloudCZ@users.noreply.github.com>
2023-11-10 12:01:00 +0100

    Reformating of the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,81 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SearchCriteria from './SearchCriteria';
 
 function App() {
-  // Start with one search criteria
-  const [showAdvancedSearch, setShowAdvancedSearch] = useState(false); 
-  const [searchCriteria, setSearchCriteria] = useState([
-    { field: '', expression: '', value: '' }
-  ]);
+    // Start with one search criteria
+    const [showAdvancedSearch, setShowAdvancedSearch] = useState(false);
+    const [searchCriteria, setSearchCriteria] = useState([
+        {field: '', expression: '', value: ''}
+    ]);
 
-  const handleCriteriaChange = (index, field, value) => {
-      const newCriteria = [...searchCriteria];
-      newCriteria[index][field] = value;
-      setSearchCriteria(newCriteria);
+    const handleCriteriaChange = (index, field, value) => {
+        const newCriteria = [...searchCriteria];
+        newCriteria[index][field] = value;
+        setSearchCriteria(newCriteria);
     };
 
     const addSearchCriteria = () => {
-      setSearchCriteria([...searchCriteria, { field: '', expression: '', value: '' }]);
+        setSearchCriteria([...searchCriteria, {field: '', expression: '', value: ''}]);
     };
 
     const removeSearchCriteria = index => {
-      const newCriteria = [...searchCriteria];
-      newCriteria.splice(index, 1);
-      setSearchCriteria(newCriteria);
+        const newCriteria = [...searchCriteria];
+        newCriteria.splice(index, 1);
+        setSearchCriteria(newCriteria);
     };
     const handleSearch = () => {
-  // Assemble the search query
-  let queryString = '';
-  for (let i = 0; i < searchCriteria.length; i++) {
-    const sc = searchCriteria[i];
-    if (i !== 0) { // Do not add the expression for the first item
-      queryString += encodeURIComponent(sc.expression) + '+';
-    }
-    queryString += `${encodeURIComponent(sc.field)}=${encodeURIComponent(sc.value)}`;
-    if (i < searchCriteria.length - 1) {
-      queryString += '+';
-    }
-  }
+        // Assemble the search query
+        let queryString = '';
+        for (let i = 0; i < searchCriteria.length; i++) {
+            const sc = searchCriteria[i];
+            if (i !== 0) { // Do not add the expression for the first item
+                queryString += encodeURIComponent(sc.expression) + '+';
+            }
+            queryString += `${encodeURIComponent(sc.field)}=${encodeURIComponent(sc.value)}`;
+            if (i < searchCriteria.length - 1) {
+                queryString += '+';
+            }
+        }
 
-  const url = `https://localhost:3000/search?q=${queryString}`;
-  window.open(url, '_blank');
-  };
+        const url = `https://localhost:3000/search?q=${queryString}`;
+        window.open(url, '_blank');
+    };
 
 
-  return (
-  <>
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-    </div>
-    {!showAdvancedSearch && (
-      <button onClick={() => setShowAdvancedSearch(true)}>Advanced Search</button>
-    )}
-    {showAdvancedSearch && (
-      <div className="search-section">
-        {searchCriteria.map((criteria, index) => (
-          <div key={index}>
-            <SearchCriteria
-              criteria={criteria}
-              onChange={(e, field) => handleCriteriaChange(index, field, e.target.value)}
-              onRemove={() => removeSearchCriteria(index)}
-              showRemoveButton={index > 0}
-            />
-          </div>
-        ))}
-        <button onClick={addSearchCriteria}>Add Field</button>
-        <button onClick={handleSearch}>Search</button>
-      </div>
-    )}
-  </>
-);
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            {!showAdvancedSearch && (
+                <button onClick={() => setShowAdvancedSearch(true)}>Advanced Search</button>
+            )}
+            {showAdvancedSearch && (
+                <div className="search-section">
+                    {searchCriteria.map((criteria, index) => (
+                        <div key={index}>
+                            <SearchCriteria
+                                criteria={criteria}
+                                onChange={(e, field) => handleCriteriaChange(index, field, e.target.value)}
+                                onRemove={() => removeSearchCriteria(index)}
+                                showRemoveButton={index > 0}
+                            />
+                        </div>
+                    ))}
+                    <button onClick={addSearchCriteria}>Add Field</button>
+                    <button onClick={handleSearch}>Search</button>
+                </div>
+            )}
+        </>
+    );
 
 }
 
--- src/SearchCriteria.jsx
+++ src/SearchCriteria.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 
-function SearchCriteria({ criteria, onChange, onRemove, showRemoveButton }) {
-  return (
-    <div className="search-criteria">
-        {showRemoveButton && (
-        <select value={criteria.expression} onChange={e => onChange(e, 'expression')}>
-            <option value=""></option>
-            <option value="AND">AND</option>
-            <option value="OR">OR</option>
-            <option value="NOT">NOT</option>
-          </select>
+function SearchCriteria({criteria, onChange, onRemove, showRemoveButton}) {
+    return (
+        <div className="search-criteria">
+            {showRemoveButton && (
+                <select value={criteria.expression} onChange={e => onChange(e, 'expression')}>
+                    <option value=""></option>
+                    <option value="AND">AND</option>
+                    <option value="OR">OR</option>
+                    <option value="NOT">NOT</option>
+                </select>
             )}
-      <select value={criteria.field} onChange={e => onChange(e, 'field')}>
-          <option value=""></option>
-          <option value="Protein">Protein</option>
-          <option value="Molecule">Molecule</option>
-          <option value="Protein">Weight</option>
-          <option value="Molecule">Height</option>
-          {/* Options for fields */}
-        </select>
-      <input
-        type="text"
-        value={criteria.value}
-        onChange={e => onChange(e, 'value')}
-      />
-      {showRemoveButton && (
-        <button onClick={onRemove}>Remove</button>
-      )}
-    </div>
-  );
+            <select value={criteria.field} onChange={e => onChange(e, 'field')}>
+                <option value=""></option>
+                <option value="Protein">Protein</option>
+                <option value="Molecule">Molecule</option>
+                <option value="Protein">Weight</option>
+                <option value="Molecule">Height</option>
+                {/* Options for fields */}
+            </select>
+            <input
+                type="text"
+                value={criteria.value}
+                onChange={e => onChange(e, 'value')}
+            />
+            {showRemoveButton && (
+                <button onClick={onRemove}>Remove</button>
+            )}
+        </div>
+    );
 }
 
 export default SearchCriteria;
