fa07e09e99e0c067b6eb31c0c306935bd9ed2e7e
DarkCloudCZ <94063126+DarkCloudCZ@users.noreply.github.com>
2023-11-15 15:04:06 +0100

    If multiple object have the same name, distinguish them.

---
+++ src/prettier_names.py
@@ -0,0 +1,39 @@
+import json
+
+def update_duplicate_pretty_names(input_file):
+    # Read the JSON data from the input file
+    with open(input_file, 'r') as file:
+        data = json.load(file)
+
+    # Create a dictionary to store pretty names and their counts
+    pretty_name_counts = {}
+    pretty_names_to_update = set()
+
+    # Scan through the data to identify duplicate pretty names
+    for item in data:
+        pretty_name = item["pretty_name"]
+
+        # Check if the pretty_name is already in the counts dictionary
+        if pretty_name in pretty_name_counts:
+            pretty_names_to_update.add(pretty_name)
+        else:
+            pretty_name_counts[pretty_name] = 1
+
+    # Iterate through the data again to update the pretty names
+    for item in data:
+        pretty_name = item["pretty_name"]
+        field_path = item["field_path"]
+
+        # Check if the pretty_name is in the set of names to update
+        if pretty_name in pretty_names_to_update:
+            parent_name = field_path.split('.')[-2]
+            new_pretty_name = f"{parent_name} {pretty_name}"
+            item["pretty_name"] = new_pretty_name
+
+    # Write the updated data back to the input file
+    with open(input_file, 'w') as file:
+        json.dump(data, file, indent=2)
+
+# Usage example:
+input_file = "output.json"
+update_duplicate_pretty_names(input_file)
