ab9f7b29a44d8c1cc8dcbe089999292f3d437453
DarkCloudCZ <94063126+DarkCloudCZ@users.noreply.github.com>
2023-11-15 22:55:57 +0100

    Edit of the pretty_names

--- src/prettier_names.py
+++ src/prettier_names.py
@@ -1,6 +1,5 @@
 import json
 
-
 def update_duplicate_pretty_names(input_file):
     # Read the JSON data from the input file
     with open(input_file, 'r') as file:
@@ -23,18 +22,22 @@ def update_duplicate_pretty_names(input_file):
     # Iterate through the data again to update the pretty names
     for item in data:
         pretty_name = item["pretty_name"]
+        pretty_name = pretty_name
         field_path = item["field_path"]
 
         # Check if the pretty_name is in the set of names to update
         if pretty_name in pretty_names_to_update:
             parent_name = field_path.split('.')[-2]
-            new_pretty_name = f"{parent_name} {pretty_name}"
+            new_pretty_name = f"{pretty_name.replace('_', ' ').capitalize()} ({parent_name.replace('_', ' ').capitalize()})"
             item["pretty_name"] = new_pretty_name
+        else:
+            new_pretty_name = f"{pretty_name.replace('_', ' ').capitalize()}"
+            item["pretty_name"] = new_pretty_name
+
 
     # Write the updated data back to the input file
     with open(input_file, 'w') as file:
         json.dump(data, file, indent=2)
-
 
 input_file = "output.json"
 update_duplicate_pretty_names(input_file)
