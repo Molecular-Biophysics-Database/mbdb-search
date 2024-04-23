4bf5b0eb7e10a2fcb809a2fe0cb03e84c10ee9a7
DarkCloudCZ <94063126+DarkCloudCZ@users.noreply.github.com>
2024-04-23 16:49:22 +0200

    Before polytypes

--- src/convert_yaml_to_json.py
+++ src/convert_yaml_to_json.py
@@ -73,8 +73,8 @@ def build_json_output(schema, base_path, defs, json_output):
 
 
 def main():
-    yaml_name = "simplified_model.yaml"
-    # yaml_name = "MST.yaml"
+    # yaml_name = "simplified_model.yaml"
+    yaml_name = "MST.yaml"
     json_name = "output.json"
 
     # Read YAML content from a file
